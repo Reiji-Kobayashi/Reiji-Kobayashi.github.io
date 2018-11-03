dd0d52e2379839afa91d60aff86a3c75492fbd86
Reiji Kobayashi <0zi.kobayashi@gmail.com>
2018-11-03 12:34:21 +0900

    group

--- group.js
+++ group.js
@@ -2,34 +2,57 @@ let localStream = null;
 let peer = null;
 let existingCall = null;
 
+let constraints = {
+    video: {},
+    audio: true
+};
+constraints.video.width = {
+    min: 320,
+    max: 320
+};
+constraints.video.height = {
+    min: 240,
+    max: 240
+};
+
 navigator.mediaDevices.getUserMedia({video: true, audio: true})
     .then(function (stream) {
-        // Success
-    $('#my-video').get(0).srcObject = stream;
-        localStream = stream;
+      // Success
+    	$('#my-video').get(0).srcObject = stream;
+      localStream = stream;
     }).catch(function (error) {
-    // Error
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-});
+    	// Error
+    	console.error('mediaDevice.getUserMedia() error:', error);
+    	return;
+		});
 
-multiparty = new MultiParty({
+peer = new Peer({
 	key: '084fb3fd-aa38-4399-860a-0d6556715b69',
-	debug: 3,
-	"reliable": true 
+	debug: 3
 });
 
-multiparty.on('open', function(){
-	$('#my-id').text(multiparty.id);
+
+peer.on('open', function(){
+	$('#my-id').text(peer.id);
 });
 
-multiparty.on('error', function(err){
+peer.on('error', function(err){
 	alert(err.message);
 });
 
+peer.on('close', function(){
+});
+
+peer.on('disconnected', function(){
+});
+
 $('#make-call').submit(function(e){
-  e.preventDefault();
-  const call = multiparty.call($('#callto-id').val(), localStream);
+	e.preventDefault();
+  let roomName = $('#join-room').val();
+  if (!roomName) {
+      return;
+  }
+  const call = peer.joinRoom(roomName, {mode: 'sfu', stream: localStream});
   setupCallEventHandlers(call);
 });
 
@@ -37,36 +60,47 @@ $('#end-call').click(function(){
   existingCall.close();
 });
 
-
+peer.on('call', function(call){
+  call.answer(localStream);
+  setupCallEventHandlers(call);
+});
 
 function setupCallEventHandlers(call){
 	if (existingCall) {
 		existingCall.close();
 	};
 
-  existingCall = call;
+	existingCall = call;
+    setupEndCallUI();
+    $('#room-id').text(call.name);
 
-  call.on('stream', function(stream){
-		addVideo(call,stream);
-		setupEndCallUI();
-		$('#their-id').text(call.remoteId);
-	});
+    call.on('stream', function(stream){
+        addVideo(stream);
+    });
 
-	call.on('close', function(){
-			removeVideo(call.remoteId);
-			setupMakeCallUI();
-	});
+    call.on('peerLeave', function(peerId){
+        removeVideo(peerId);
+    });
+
+    call.on('close', function(){
+        removeAllRemoteVideos();
+        setupMakeCallUI();
+    });
 }
 
-// サーバとpeerに接続
-multiparty.start()
-
 function addVideo(call,stream){
-    $('#their-video').get(0).srcObject = stream;
+	const videoDom = $('<video autoplay>');
+	videoDom.attr('id',stream.peerId);
+	videoDom.get(0).srcObject = stream;
+	$('.videosContainer').append(videoDom);
 }
 
 function removeVideo(peerId){
      $('#' + peerId).remove();
+}
+
+function removeAllRemoteVideos(){
+    $('.videosContainer').empty();
 }
 
 function setupMakeCallUI(){
